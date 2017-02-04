a114c0314b0424efab3636b6c14e48e244d82749
hidenori-t <hidenori.tatara@gmail.com>
2017-02-05 00:00:10 +0900

    add comments

--- ch01/man.py
+++ ch01/man.py
@@ -1,18 +1,20 @@
 # coding: utf-8
-# 1.4.2 クラス pp.10-116
+# 1.4.2 クラス pp.10-11
+# 『みんなのPython』p.253 参考
+
 class Man:
     """サンプルクラス"""
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name): # 初期化メソッドを定義=インスタンスを作る, 追加の引数 name を設定
+        self.name = name # instance (self) に，attribute (name) にnameを代入して追加
         print("Initilized!")
 
-    def hello(self):
+    def hello(self): # hello() method を定義
         print("Hello " + self.name + "!")
 
-    def goodbye(self):
+    def goodbye(self): # goodbye() method を定義
         print("Good-bye " + self.name + "!")
 
-m = Man("David") # Man クラスから m というインスタンス(オブジェクト)を生成
-m.hello()
-m.goodbye()
+m = Man("David") # Man Class から instance (m) を生成
+m.hello() # instance (m)の hello() method を使う
+m.goodbye() # instance (m)の goodbye() method を使う
--- ch04/gradient_simplenet.py
+++ ch04/gradient_simplenet.py
@@ -8,13 +8,13 @@ from common.gradient import numerical_gradient
 
 
 class simpleNet:
-    def __init__(self): # 初期化用 method=constructor
-        self.W = np.random.randn(2,3) # 標準正規分布による 2x3 の行列の乱数
+    def __init__(self): # 初期化用 method=constructor=インスタンスを作る
+        self.W = np.random.randn(2,3) # instance (self) に，attribute (W) に標準正規分布による 2x3 の行列の乱数を代入して追加
 
-    def predict(self, x):
+    def predict(self, x): # 予測するための method (predict) を定義 cf.p.76
         return np.dot(x, self.W)
 
-    def loss(self, x, t):
+    def loss(self, x, t): # 損失関数を求める method (loss) を定義
         z = self.predict(x)
         y = softmax(z)
         loss = cross_entropy_error(y, t)
@@ -24,7 +24,7 @@ class simpleNet:
 x = np.array([0.6, 0.9])
 t = np.array([0, 0, 1])
 
-net = simpleNet()
+net = simpleNet() # simpleNet Class から instance (net) を生成
 
 # 勾配を求める
 f = lambda w: net.loss(x, t) # 損失関数を計算する新しい関数 f を定義
