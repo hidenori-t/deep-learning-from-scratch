d52ad61a1fce53805d0b44f83a2fe20ba23d8e75
hidenori-t <hidenori.tatara@gmail.com>
2017-02-11 21:32:23 +0900

    add comments

--- ch05/buy_apple_orange.py
+++ ch05/buy_apple_orange.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+# p.140
 from layer_naive import *
 
 apple = 100
@@ -8,10 +9,10 @@ orange_num = 3
 tax = 1.1
 
 # layer
-mul_apple_layer = MulLayer()
-mul_orange_layer = MulLayer()
-add_apple_orange_layer = AddLayer()
-mul_tax_layer = MulLayer()
+mul_apple_layer = MulLayer() # (1)
+mul_orange_layer = MulLayer() # (2)
+add_apple_orange_layer = AddLayer() # (3)
+mul_tax_layer = MulLayer() # (4)
 
 # forward
 apple_price = mul_apple_layer.forward(apple, apple_num)  # (1)
@@ -19,7 +20,7 @@ orange_price = mul_orange_layer.forward(orange, orange_num)  # (2)
 all_price = add_apple_orange_layer.forward(apple_price, orange_price)  # (3)
 price = mul_tax_layer.forward(all_price, tax)  # (4)
 
-# backward
+# backward 順伝播と逆の順番で逆伝播のメソッドを呼び出す
 dprice = 1
 dall_price, dtax = mul_tax_layer.backward(dprice)  # (4)
 dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price)  # (3)
